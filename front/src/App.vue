d1c131ef7756b07ce4dca4ec15680fb1620954a7
Antonstockmarr <s164170@student.dtu.dk>
2020-07-09 12:03:22 +0200

    data visualization

--- front/src/App.vue
+++ front/src/App.vue
@@ -59,6 +59,7 @@ a {
 }
 
 #data-box {
-  margin: 20px 20px;
+  margin: 20px 20px 0;
+  padding-bottom: 20px;
 }
 </style>
--- front/src/components/City.vue
+++ front/src/components/City.vue
@@ -1,40 +1,77 @@
 <template>
-  <div id="city" @click="expand()">
-      <div>{{this.name}}</div>
-      <div v-if="this.expanded">
-          <div v-bind:key="measurement.id" v-for="measurement in this.measurements">
-              {{measurement.value}}
-          </div>
-      </div>
-  </div>
+    <div id="city" @click="expand()">
+        <div>{{this.name}}</div>
+        <div id="chart-container" v-if="this.expanded">
+            <AirChart v-if="this.rendered" id="chart" v-bind="chart"/>
+        </div>
+    </div>
 </template>
 
 <script>
 import axios from 'axios';
-//import Chart from 'chart.js'
+//import chart from 'chart.js'
+import AirChart from './AirChart'
 
 
 export default {
     name: 'City',
     props: ['city','name','id'],
     components: {
-
+        AirChart
     },
     data() {
-    return {
-        expanded: false,
-        measurements: []
-    }
+        return {
+            expanded: false,
+            rendered: false,
+            chart: {chartdata: {}, options: {}}
+        }
     },
     methods: {
     expand() {
-        if (this.measurements.length===0) {
+        if (!this.rendered) {
             const url = 'https://api.openaq.org/v1/measurements?city='+this.city;
             axios.get(url)
-                .then(response => this.measurements = response.data.results)
+                .then(response => this.parseData(response.data.results))
+                .then(measurements => this.configureChart(measurements))
+                .then(() => this.rendered = true)
                 .catch(function(err) { console.log(err)});
         }
         this.expanded = !this.expanded;
+    },
+    parseData(result) {
+        let measurements = {
+            values: [],
+            dates: [],
+            unit: ''
+            }
+        measurements.values = result.map(measurement => measurement.value);
+        measurements.dates = result.map(measurement => measurement.date.local);
+        if (result.length != 0) {
+            measurements.unit = result[0].unit;
+        }
+        return measurements;
+    },
+    configureChart(measurements) {
+        this.chart.chartdata = {
+            labels: measurements.dates,
+            datasets: [{
+                label: 'Pollution ['+measurements.unit+']',
+                backgroundColor: '#f87979',
+                data: measurements.values,
+                borderWidth: 1
+            }]
+        };
+        this.chart.options = {
+            responsive:true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
     }
   }
 }
@@ -49,6 +86,17 @@ export default {
     padding: 5px;
     margin: 5px 0;
     font-size: 24px;
+}
+
+#chart-container {
+    padding: 10px 30px 0;
+    width: 100%;
+    height: 400px;
+}
+
+#chart {
+    width: calc(100% - 60px);
+    height: calc(100% - 10px);
 }
 
 
